8dda4506f84219f511ba97ab2fb6626ac691919f
victorfonseca <victortales@gmail.com>
2015-06-10 16:42:43 -0300

    Push!

---
+++ src/main/java/br/com/caixaseguradora/CaixaSeguradoraApplication.java
@@ -0,0 +1,12 @@
+package br.com.caixaseguradora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaSeguradoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaixaSeguradoraApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/caixaseguradora/CaixaSeguradoraApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.caixaseguradora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CaixaSeguradoraApplication.class)
+public class CaixaSeguradoraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
