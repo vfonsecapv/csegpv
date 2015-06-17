810705714c5b24d3f61fd390e0d31797807968fc
Bernowly <bernowly@gmail.com>
2015-06-17 16:53:54 -0300

    alteracao retorno

--- caixaseguradora-client-produto/src/main/groovy/caixaseguradora/ProdutoRepositoryFactory.groovy
+++ caixaseguradora-client-produto/src/main/groovy/caixaseguradora/ProdutoRepositoryFactory.groovy
@@ -4,15 +4,11 @@ import feign.Feign
 import feign.jackson.JacksonDecoder
 
 class ProdutoRepositoryFactory {
-	def create(String url) {	
+	static def create(String url) {	
 		ProdutoRepository produtoRepository = Feign.builder()
 				.decoder(new JacksonDecoder())
 				.target(ProdutoRepository.class, url)
-		produtoRepository.getProdutos()
-	}
-
-	def create(URL url) {
-		return create(url.toString());
+		produtoRepository
 	}
 }
 
