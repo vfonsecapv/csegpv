08d5dbe5bcb0a36f32b550cf4f777534e0eeef41
victorfonseca <victortales@gmail.com>
2015-06-17 17:49:12 -0300

    .

--- caixaseguradora-ui/src/main/groovy/caixaseguradora/config/DefaultConfiguration.groovy
+++ caixaseguradora-ui/src/main/groovy/caixaseguradora/config/DefaultConfiguration.groovy
@@ -1,11 +1,9 @@
 package caixaseguradora.config
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
+import org.springframework.context.annotation.*
 
-import caixaseguradora.client.ProdutoRepository
-import caixaseguradora.client.ProdutoRepositoryFactory
+import caixaseguradora.client.*
 
 @Configuration
 @Profile("default")
@@ -15,5 +13,4 @@ class DefaultConfiguration {
 	ProdutoRepository cityRepository() {
 		new ProdutoRepositoryFactory().create("http://192.168.100.7:9090")
 	}
-
 }
--- caixaseguradora-ui/src/main/groovy/caixaseguradora/controller/ProdutoController.groovy
+++ caixaseguradora-ui/src/main/groovy/caixaseguradora/controller/ProdutoController.groovy
@@ -1,9 +1,7 @@
 package caixaseguradora.controller
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 import caixaseguradora.client.ProdutoRepository
 
@@ -22,7 +20,4 @@ public class ProdutoController {
 	def listarProdutos() {
 		repository.getProdutos()
 	}
-
 }
-
-
