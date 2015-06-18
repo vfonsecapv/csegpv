ada4650fe18ffd57f02a53931ef24e233d57f405
victorfonseca <victortales@gmail.com>
2015-06-17 23:31:54 -0300

    Histryx

--- caixaseguradora-hystrix-dashboard/src/main/groovy/caixaseguradora/hystrix/CaixaSeguradoraApplication.groovy
+++ caixaseguradora-hystrix-dashboard/src/main/groovy/caixaseguradora/hystrix/CaixaSeguradoraApplication.groovy
@@ -1,14 +1,19 @@
-package caixaseguradora
+package caixaseguradora.hystrix
 
 import org.springframework.web.bind.annotation.*
 import org.springframework.boot.*
 import org.springframework.boot.autoconfigure.*
 
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.stereotype.Controller
+
 @SpringBootApplication
+@Controller
+@EnableHystrixDashboard
 class CaixaSeguradoraApplication {
 	@RequestMapping("/")
-	public String home() {
-		"forward:/hystrix";
+	def home() {
+		 "forward:/hystrix"
 	}
 
 	static void main(String... args) {
--- caixaseguradora-service-produto/src/main/groovy/caixaseguradora/service/controller/ProdutoController.groovy
+++ caixaseguradora-service-produto/src/main/groovy/caixaseguradora/service/controller/ProdutoController.groovy
@@ -4,21 +4,32 @@ import caixaseguradora.service.model.Produto
 
 import org.springframework.web.bind.annotation.*
 import org.springframework.beans.factory.annotation.*
+import org.springframework.context.annotation.*
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
+
+import org.springframework.cloud.netflix.hystrix.EnableHystrix
 
 import caixaseguradora.data.RedisConfig
 
+@Configuration
+@EnableAutoConfiguration
+@EnableHystrix
 @RestController
 @RequestMapping("/produto")
 class ProdutoController {
 	@Autowired
 	RedisConfig redis
 
+	@HystrixCommand
 	@RequestMapping(method=RequestMethod.POST)
 	@ResponseBody
 	newProduto(@RequestBody Produto produto) {
 		redis.setEntity("prd:" + produto.id, produto)
 	}
 
+	@HystrixCommand
 	@RequestMapping("/produtos")
 	@ResponseBody
 	getAllProdutos() {
@@ -32,12 +43,14 @@ class ProdutoController {
 		produtos
 	}
 
+	@HystrixCommand
 	@RequestMapping("/prd/{key}")
 	@ResponseBody
 	getProdutoByKey(@PathVariable("key") String key) {
 		redis.getEntity(Produto.class, "prd:" + key)
 	}
 
+	@HystrixCommand
 	@RequestMapping("/del/{key}")
 	@ResponseBody
 	deleteProdutoByKey(@PathVariable("key") String key) {
