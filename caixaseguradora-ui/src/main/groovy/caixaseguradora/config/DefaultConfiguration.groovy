01676ca6ef580e0384d9d87304bfdb3afaf17020
victorfonseca <victortales@gmail.com>
2015-06-18 10:30:08 -0300

    .

--- caixaseguradora-ui/src/main/groovy/caixaseguradora/config/DefaultConfiguration.groovy
+++ caixaseguradora-ui/src/main/groovy/caixaseguradora/config/DefaultConfiguration.groovy
@@ -11,12 +11,7 @@ class DefaultConfiguration {
 	private String url = "http://192.168.100.7:9090";
 	
 	@Bean
-<<<<<<< HEAD
 	ProdutoRepository cityRepository() {
 		new ProdutoRepositoryFactory().create(url)
-=======
-	ProdutoRepository produtoRepository() {
-		new ProdutoRepositoryFactory().create("http://localhost:8989")
->>>>>>> 0bfa94eed77fe4013d99d29693fab07a236fb6f1
 	}
 }
