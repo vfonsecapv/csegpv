31c0bed81038e9624f3727fffe14665828ad82af
victorfonseca <victortales@gmail.com>
2015-06-19 14:06:16 -0300

    .

---
+++ caixaseguradora-ui/src/main/groovy/caixaseguradora/AppInitializer.groovy
@@ -0,0 +1,12 @@
+package caixaseguradora
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
+
+class AppInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        application.sources(Application.class);
+    }
+}
