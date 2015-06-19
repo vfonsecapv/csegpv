ecfa34aea79aea16c6a82b284c15f769c062caec
victorfonseca <victortales@gmail.com>
2015-06-19 18:20:20 -0300

    .

--- caixaseguradora-ui/src/main/groovy/caixaseguradora/config/CloudConfiguration.groovy
+++ caixaseguradora-ui/src/main/groovy/caixaseguradora/config/CloudConfiguration.groovy
@@ -7,11 +7,8 @@ import org.springframework.context.annotation.Profile
 
 import caixaseguradora.client.ProdutoRepository
 
-import org.springframework.cloud.config.java.*
-
 @Configuration
 @Profile("cloud")
-@ServiceScan
 class CloudConfiguration extends AbstractCloudConfig {	
 
     @Bean
